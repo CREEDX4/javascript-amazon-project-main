e661821a771c23ac8e3fbeb1c4d2d374f41d9324
Sudo <diademebenezer@gmail.com>
2026-03-18 14:19:17 +0100

    remove default test and add money test

---
+++ javascript-amazon-project-main/tests-jasmine/Money-test-jasmine.js
@@ -0,0 +1,22 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+describe('test suite: formatCurrency', () => {
+    it('converts cents into dollars', () => {
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+
+    it('handles edge case 0', () => {
+        expect(formatCurrency(0)).toEqual('0.00');
+    }); 
+
+    it ('handles edge case 2000.5', () => {
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+});
+
+
+if (formatCurrency(2000.5)=== '20.01') {
+    console.log('passed');
+} else {
+    console.log('failed');
+}
