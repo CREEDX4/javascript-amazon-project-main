652222b3a72c05bc719a4ec694bc49b32428abc3
Sudo <diademebenezer@gmail.com>
2026-03-20 08:49:09 +0100

    create automated tests for addToCart

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,24 +1,32 @@
-let storedCart = null;
+export let cart;
+export let storedCart;
 
-try {
-  storedCart = JSON.parse(localStorage.getItem('cart'));
-} catch (error) {
-  console.warn('Failed to parse cart from localStorage, resetting to default.', error);
-}
 
-export let cart = Array.isArray(storedCart)
-  ? storedCart
-  : [{
-      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-      quantity: 2,
-      deliveryOptionId: '1'
-    },
-    {
-      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-      quantity: 1,
-      deliveryOptionId: '2'
+loadFromStorage();
+
+  export function loadFromStorage(){
+    let storedCart = null;
+
+    try {
+    storedCart = JSON.parse(localStorage.getItem('cart'));
+    } catch (error) {
+    console.log('Failed to parse cart from localStorage, resetting to default.', error);
     }
-  ];
+
+    cart = Array.isArray(storedCart)
+    ? storedCart
+    : [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionId: '1'
+        },
+        {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionId: '2'
+        }
+    ];
+  };
 
 
 function saveToStorage(){
